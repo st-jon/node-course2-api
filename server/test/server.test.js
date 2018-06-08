7931421a47957b1b3e4431eb49748a3b0ce42166
Christophe Johanny <c.johanny@gmail.com>
2018-06-08 15:35:51 +0200

    add delete test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -95,33 +95,43 @@ describe('GET /todos/:id', () => {
     it('Should return 404 if the ID is not valid', (done) => {
         request(app)
             .get(`/todos/123`)
-            .expect(400)
+            .expect(404)
             .end(done)
     })
 })
 
-// describe('DELETE /todos/:id', () => {
-//     it('Should delete todo doc', (done) => {
-//         request(app)
-//             .delete(`/todos/${todos[1]._id.toHexString()}`)
-//             .expect(200)
-//             .expect((res) => {
-//                 expect(res.body.todo.text).toBe('Second test todo')
-//             })
-//             .end(done)
-//     })
+describe('DELETE /todos/:id', () => {
+    it('Should delete todo doc', (done) => {
+        let hexID = todos[1]._id.toHexString()
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe('Second test todo')
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err)
+                }
 
-//     it('Should return 404 if todo not found', (done) => {
-//         request(app)
-//             .delete(`/todos/${new ObjectID().toHexString()}`)
-//             .expect(404)
-//             .end(done)
-//     })
+                Todo.findById(hexID).then((todo) => {
+                    expect(todo).toNotExist()
+                    done()
+                }).catch((e) => done(e))
+            })
+    })
 
-//     it('Should return 404 if the ID is not valid', (done) => {
-//         request(app)
-//             .delete(`/todos/123`)
-//             .expect(400)
-//             .end(done)
-//     })
-// })
+    it('Should return 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done)
+    })
+
+    it('Should return 404 if the ID is not valid', (done) => {
+        request(app)
+            .delete(`/todos/123`)
+            .expect(404)
+            .end(done)
+    })
+})
