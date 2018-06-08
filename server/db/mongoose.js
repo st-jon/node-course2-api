00c3ca384609d200eab88a6ae7818fcefcdfabe6
Christophe Johanny <c.johanny@gmail.com>
2018-06-08 14:00:31 +0200

    change to mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,7 @@ let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://st-jon:1dealcrash@ds247430.mlab.com:47430/todo-app-api-st-jon'
-  };
-  mongoose.connect( db.localhost || db.mlab)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 
 module.exports = {
